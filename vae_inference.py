775116dd58d78b3d140fb270dcc4a7c7fc098946
Ryan <1018718155@qq.com>
2022-11-07 20:41:45 -0800

    for generate feature file

--- vae_inference.py
+++ vae_inference.py
@@ -1,31 +1,28 @@
 import torch
 import os
-import cv2
+from tqdm import tqdm
+
+from dataloader import CelebA
 from vae import VAE
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 classifier = VAE().to(DEVICE)
-load_pickle = "vae_finetune_epoch_9_0.0089038.pickle"
+load_pickle = "vae_10__epoch_16_0.008683.pickle"
 if os.path.exists("pickles/" + load_pickle):
     with open("pickles/" + load_pickle, "rb") as f:
         state_dict = torch.load(f, map_location=DEVICE)
     classifier.load_state_dict(state_dict)
 
-image_path = r"D:\celebA\img_align_celeba_png\000001_f.png"
-# image_path = r"D:\celebA\img_align_celeba_png\000003_m.png"
+dataset = CelebA("D:\celebA\img_align_celeba_png")
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=False)
 
+with open("vae_feature_record.txt", "w") as f:
+    for i, (x, label, im_name) in tqdm(enumerate(dataloader)):
+        x = x.cuda()
+        with torch.no_grad():
+            y, mu, log_var = classifier(x)
 
-im = cv2.imread(image_path)
-im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-im = cv2.resize(im, (160, 192))
-im = im.astype('float32') / 255
-im = im.transpose(2, 0, 1)
-torch_im = torch.from_numpy(im).float()
-torch_im = torch_im.unsqueeze(0)
-torch_im = torch_im.to(DEVICE)
-
-with torch.no_grad():
-    y, mu, log_var = classifier(torch_im)
-
-print(y, mu, log_var)
+        BS = x.shape[0]
+        for j in range(BS):
+            f.write(f"{im_name[j]}  {label[j].item()}  {mu[j].cpu().numpy().tolist()}\n")

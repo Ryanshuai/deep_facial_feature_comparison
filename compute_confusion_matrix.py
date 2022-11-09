9c1a908a90fab770f67f086b9f7fd920b2f438a1
Ryan <1018718155@qq.com>
2022-11-08 17:24:49 -0800

    confusion matrix

--- compute_confusion_matrix.py
+++ compute_confusion_matrix.py
@@ -7,6 +7,12 @@ lines = [line.strip() for line in lines]
 y_label = [int(line.split("  ")[1]) for line in lines]
 y_pred = [int(float(line.split("  ")[2]) > 0) for line in lines]
 
+dataset_length = len(y_label)
+test_set_length = int(dataset_length * 0.2)
+val_set_length = int(dataset_length * 0.1)
+y_label = y_label[-(test_set_length + val_set_length):-val_set_length]
+y_pred = y_pred[-(test_set_length + val_set_length):-val_set_length]
+
 print("Accuracy: \n", accuracy_score(y_label, y_pred))
 print("Confusion Matrix: \n", confusion_matrix(y_label, y_pred))
 print("Classification Report: \n", classification_report(y_label, y_pred))

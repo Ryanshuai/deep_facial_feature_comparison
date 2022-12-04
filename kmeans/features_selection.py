59eb15ffd8d2eb6537fae9bf167ca1d070650c8b
liuchenxi <liuchenxi@ainirobot.com>
2022-12-04 12:47:43 -0800

    Add algorithms for distances and geature selection

--- kmeans/features_selection.py
+++ kmeans/features_selection.py
@@ -11,7 +11,7 @@ def loadData(fileName):
     i = 0
     while True:
         i+=1
-        if(i > 50):
+        if(i > 5000):
             break
         line = file.readline()
         if not line:
@@ -31,18 +31,90 @@ def loadData(fileName):
 
     return imgNames, np.array(imgPreds, dtype=np.int8), np.array(imgValues, dtype=np.float64), np.array(imgFeatures, dtype=np.float64)
 
+def varianceSelection(trainX, trainY):
+    selectedFeatures = []
+    (m, n) = trainX.shape
+    variances = []
+    for i in range(0, n):
+        sampleX = trainX[:, i]
+        mean = sampleX.mean()
+        # variance = np.sqrt(np.sum(np.square(sampleX - mean), axis=0)/(m - 1))
+        # v1 = sampleX - mean
+        # v2 = np.square(sampleX - mean)
+        # v3 = np.sum(np.square(sampleX - mean), axis=0)
+        variance = np.sum(np.square(sampleX - mean), axis=0)/(m - 1)
+        variances.append(variance)
+    sortedVariances = np.argsort(np.array(variances))
+    threshhold = 50.0
+    for i in range(0, n):
+        if(variances[sortedVariances[i]] >= threshhold):
+            selectedFeatures.append(sortedVariances[i])
+            print("variance " + str(sortedVariances[i]) + ": " + str(variances[sortedVariances[i]]) + "\n")
+    print("varianceSelection selectedFeatures are " + str(selectedFeatures) + "\n")
+    return selectedFeatures
 
-# def nnAlg(trainX, trainY, featuresIdxs):
-#     predY = []
-#     trainXTemp = trainX[:, featuresIdxs]
-#     for k in range(0, len(trainX)):
-#         sampleX = np.array([trainX[k]])
-#         sampleXTemp = sampleX[:, featuresIdxs]
-#         eucDises = np.sum(np.abs(trainXTemp - sampleXTemp), axis=1)
-#         # eucDises = np.sqrt(np.sum(np.square(trainXTemp - sampleXTemp), axis=1))
-#         sortedEucDises = np.argsort(eucDises)
-#         predY.append(trainY[sortedEucDises[1]])
-#     return predY
+def pearsonCorrelationSelection(trainX, trainY):
+    selectedFeatures = []
+    (m, n) = trainX.shape
+    meanY = trainY.mean()
+    rs = []
+    for i in range(0, n):
+        sampleX = trainX[:, i]
+        meanX = sampleX.mean()
+        numerator = np.dot((sampleX - meanX), (trainY - meanY))
+        denominator = np.sum(np.square(sampleX - meanX), axis=0)*np.sum(np.square(trainY - meanY), axis=0)
+        r = numerator/denominator
+        rs.append(r)
+    sortedRs = np.argsort(np.array(rs))
+    k = 10
+    for i in range(0, k):
+        print("pearsonr " + str(sortedRs[n - i - 1]) + ": " + str(rs[sortedRs[n - i - 1]]) + "\n")
+        selectedFeatures.append(sortedRs[n - i - 1])
+    return selectedFeatures
+
+def fisherScoreSelection(trainX, trainY):
+    selectedFeatures = []
+    (m, n) = trainX.shape
+    trainX0 = []
+    trainX1 = []
+    for j in range(0, m):
+        if(trainY[j] == 0):
+            trainX0.append(trainX[j])
+        else:
+            trainX1.append(trainX[j])
+    trainX0 = np.array(trainX0)
+    trainX1 = np.array(trainX1)
+    scores = []
+    for i in range(0, n):
+        sampleX = trainX[:, i]
+        sampleX0 = trainX0[:, i]
+        sampleX1 = trainX1[:, i]
+        mean = sampleX.mean()
+        mean0 = sampleX0.mean()
+        mean1 = sampleX1.mean()
+        m0 = sampleX0.size
+        m1 = sampleX1.size
+        # v0 = sampleX0 - mean0
+        v0 = np.sum(np.square(sampleX0 - mean0), axis=0)/(m0 - 1)
+        v1 = np.sum(np.square(sampleX1 - mean1), axis=0)/(m1 - 1)
+        numerator = m0*np.square(mean0 - mean) + m1*np.square(mean1 - mean)
+        denominator = m0*np.square(v0) + m1*np.square(v1)
+        score = numerator / denominator
+        scores.append(score)
+    sortedScores = np.argsort(np.array(scores))
+    score = 1.0
+    for i in range(0, n):
+        if(scores[sortedScores[n - i - 1]] >= 1.0):
+            print("fisherScoreSelection " + str(sortedScores[n - i - 1]) + ": " + str(scores[sortedScores[n - i - 1]]) + "\n")
+            selectedFeatures.append(sortedScores[n - i - 1])
+
+    return selectedFeatures
+
+def forwardSelection(trainX, trainY):
+    return selection(trainX, trainY, 1)
+
+def backwardSelection(trainX, trainY):
+    return selection(trainX, trainY, 2)
 
 def nnAlg(trainX, trainY, featuresIdxs):
     predY = []
@@ -77,33 +149,51 @@ def featureInList(feature, featureList):
             return True
     return False
 
-def selection(trainX, trainY):
+def selection(trainX, trainY, dirction):
     bestLvlFeatureList = []
     bestUpperLvlFeatureList = []
     bestLowerLvlFeatureList = []
     bestFeatureList = []
     (m, n) = trainX.shape
-    for j in range(0, n):
-        # if (j > 20):
-        #     break
-        lvlErrRate = 1
+    if (dirction == 1):
+        for j in range(0, n):
+            lvlErrRate = 1
+            for i in range(0, n):
+                if (featureInList(i, bestUpperLvlFeatureList)):
+                    continue
+                tempFeatureList = bestUpperLvlFeatureList[:]
+                tempFeatureList.append(i)
+                errRate = errorRate(trainY, nnAlg(trainX, trainY, tempFeatureList))
+                print("    Using feature(s) " + str(
+                    np.sort(tempFeatureList, kind='quicksort', order=None)) + " accuracy is " + str(
+                    (1 - errRate) * 100) + "%")
+                if (errRate < lvlErrRate):
+                    lvlErrRate = errRate
+                    bestLvlFeatureList = tempFeatureList
+            print("Best forward feature set is " + str(
+                np.sort(bestLvlFeatureList, kind='quicksort', order=None)) + " accuracy is " + str((1 - lvlErrRate) * 100) + "%")
+            bestUpperLvlFeatureList = bestLvlFeatureList
+            bestFeatureList.insert(0, [lvlErrRate, np.sort(bestLvlFeatureList, kind='quicksort', order=None)])
+    else:
         for i in range(0, n):
-            if (featureInList(i, bestUpperLvlFeatureList)):
-                continue
-            tempFeatureList = bestUpperLvlFeatureList[:]
-            tempFeatureList.append(i)
-            errRate = errorRate(trainY, nnAlg(trainX, trainY, tempFeatureList))
-            # print("    Using feature(s) " + str(
-            #     np.sort(tempFeatureList, kind='quicksort', order=None)) + " accuracy is " + str(
-            #     (1 - errRate) * 100.0) + "%")
-            if (errRate < lvlErrRate):
-                lvlErrRate = errRate
-                bestLvlFeatureList = tempFeatureList
-        print("Feature set #"+ str(j) + ":" + str(
-            np.sort(bestLvlFeatureList, kind='quicksort', order=None)) + " Accuracy: " + str(
-            (1 - lvlErrRate) * 100) + "%")
-        bestUpperLvlFeatureList = bestLvlFeatureList
-        bestFeatureList.insert(0, [lvlErrRate, np.sort(bestLvlFeatureList, kind='quicksort', order=None)])
+            bestLowerLvlFeatureList.append(i)
+        lvlErrRate = errorRate(trainY, nnAlg(trainX, trainY,
+                                             bestLowerLvlFeatureList))  # Calculate the error rate with all features
+        bestFeatureList.append([lvlErrRate, bestLowerLvlFeatureList])
+        for j in range(0, n - 1):
+            lvlErrRate = 1
+            for i in range(0, (n - j)):
+                tempFeatureList = bestLowerLvlFeatureList[:]
+                del tempFeatureList[i]
+                errRate = errorRate(trainY, nnAlg(trainX, trainY, tempFeatureList))
+                print("    Using feature(s) " + str(tempFeatureList) + " accuracy is " + str((1 - errRate) * 100) + "%")
+                if (errRate < lvlErrRate):
+                    lvlErrRate = errRate
+                    bestLvlFeatureList = tempFeatureList
+            print("Best backward feature set is " + str(bestLvlFeatureList) + " accuracy is " + str(
+                (1 - lvlErrRate) * 100) + "%")
+            bestLowerLvlFeatureList = bestLvlFeatureList
+            bestFeatureList.insert(0, [lvlErrRate, bestLvlFeatureList])
     errRate = 1
     idx = -1
     for i in range(0, len(bestFeatureList)):
@@ -111,9 +201,47 @@ def selection(trainX, trainY):
         if (errRate > tempFeatureList[0]):
             errRate = tempFeatureList[0]
             idx = i
-    print("The best featrue set is " + str(bestFeatureList[idx][1]) + " Accuracy: " + str(
+    print("Finish searchinng. The best featrue set is " + str(bestFeatureList[idx][1]) + " accuracy is " + str(
         (1 - bestFeatureList[idx][0]) * 100) + "%")
     return np.array(bestFeatureList, dtype=object)
+
+# def selection(trainX, trainY, direction):
+#     bestLvlFeatureList = []
+#     bestUpperLvlFeatureList = []
+#     bestLowerLvlFeatureList = []
+#     bestFeatureList = []
+#     (m, n) = trainX.shape
+#     for j in range(0, n):
+#         # if (j > 20):
+#         #     break
+#         lvlErrRate = 1
+#         for i in range(0, n):
+#             if (featureInList(i, bestUpperLvlFeatureList)):
+#                 continue
+#             tempFeatureList = bestUpperLvlFeatureList[:]
+#             tempFeatureList.append(i)
+#             errRate = errorRate(trainY, nnAlg(trainX, trainY, tempFeatureList))
+#             # print("    Using feature(s) " + str(
+#             #     np.sort(tempFeatureList, kind='quicksort', order=None)) + " accuracy is " + str(
+#             #     (1 - errRate) * 100.0) + "%")
+#             if (errRate < lvlErrRate):
+#                 lvlErrRate = errRate
+#                 bestLvlFeatureList = tempFeatureList
+#         print("Feature set #"+ str(j) + ":" + str(
+#             np.sort(bestLvlFeatureList, kind='quicksort', order=None)) + " Accuracy: " + str(
+#             (1 - lvlErrRate) * 100) + "%")
+#         bestUpperLvlFeatureList = bestLvlFeatureList
+#         bestFeatureList.insert(0, [lvlErrRate, np.sort(bestLvlFeatureList, kind='quicksort', order=None)])
+#     errRate = 1
+#     idx = -1
+#     for i in range(0, len(bestFeatureList)):
+#         tempFeatureList = bestFeatureList[i]
+#         if (errRate > tempFeatureList[0]):
+#             errRate = tempFeatureList[0]
+#             idx = i
+#     print("The best featrue set is " + str(bestFeatureList[idx][1]) + " Accuracy: " + str(
+#         (1 - bestFeatureList[idx][0]) * 100) + "%")
+#     return np.array(bestFeatureList, dtype=object)
 
 def plotChart(bestFeatureList):
     x_data = []
@@ -149,10 +277,14 @@ def plotChart(bestFeatureList):
 
 
 if __name__ == '__main__':
-    imgNames, imgPreds, imgValues, imgFeatures = loadData("classifier_feature_record.txt")
+    imgNames, imgPreds, imgValues, imgFeatures = loadData("/Users/cxliu/Documents/Code/CS235/deep_facial_feature_comparison/feature_selection/classifier_feature_record.txt")
     start = time.time()
-    bestFeatureList = selection(imgFeatures, imgPreds)
+    # varianceSelection(imgFeatures, imgPreds)
+    # pearsonCorrelationSelection(imgFeatures, imgPreds)
+    # fisherScoreSelection(imgFeatures, imgPreds)
+    # forwardSelection(imgFeatures, imgPreds)
+    backwardSelection(imgFeatures, imgPreds)
     end = time.time()
     print("time:" + str(end - start))
-    plotChart(bestFeatureList)
+    # plotChart(bestFeatureList)
     print("done\n")
--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -2,6 +2,14 @@ import numpy as np
 import random 
 import matplotlib.pyplot as plt 
 import time
+import sys
+sys.path.append('../features_selection')
+import features_selection
+from features_selection import varianceSelection
+from features_selection import pearsonCorrelationSelection
+from features_selection import fisherScoreSelection
+from features_selection import forwardSelection
+from features_selection import backwardSelection
 
 def loadData(fileName):
     imgNames = []
@@ -12,8 +20,8 @@ def loadData(fileName):
     i = 0
     while True:
         i+=1
-        # if(i > 1000):
-        #     break
+        if(i > 1000):
+            break
         line = file.readline()
         if not line:
             break
@@ -32,15 +40,53 @@ def loadData(fileName):
 
     return imgNames, np.array(imgPreds, dtype=np.int8), np.array(imgValues, dtype=np.float64), np.array(imgFeatures, dtype=np.float64)
 
-def distance(data, center):
-    temp = data - center / 2
-    eucDises = np.sqrt(np.sum(np.square(data - center), axis=1))
-    return eucDises
+def manhattanDistance(data, center):
+    # dises = np.linalg.norm(data - center, ord=1)
+    dises = np.sum(np.abs(data - center), axis=1)
+    return dises
 
-def distance2(point1, point2):
-    return np.sqrt(np.sum(np.square(point1 - point2)))
+def manhattanP2PDistance(data, center):
+    dis = np.sum(np.abs(data - center))
+    return dis
 
-def k_means(data, k, max_iter=300):
+def euclideanDistance(data, center):
+    dises = np.sqrt(np.sum(np.square(data - center), axis=1))
+    return dises
+
+def euclideanP2PDistance(data, center):
+    dis = np.sqrt(np.sum(np.square(data - center)))
+    return dis
+
+def chebyshevDistance(data, center):
+    dises = np.abs(data - center).max()
+    return dises
+
+def chebyshevP2PDistance(data, center):
+    dises = np.abs(data - center).max()
+    return dises
+
+def chebyshevDistance2(data, center):
+    temp = data - center
+    dises = np.linalg.norm(data - center, ord=np.inf)
+    return dises
+
+def chebyshevP2PDistance2(data, center):
+    temp = data - center
+    dises = np.linalg.norm(data - center, ord=np.inf)
+    return dises
+
+def cosineDistance(data, center):
+    dises = np.dot(data,center)/(np.linalg.norm(data)*(np.linalg.norm(center)))
+    return dises
+
+def cosineP2PDistance(data, center):
+    dises = np.dot(data,center)/(np.linalg.norm(data)*(np.linalg.norm(center)))
+    return dises
+
+# def distance2(point1, point2):
+#     return np.sqrt(np.sum(np.square(point1 - point2)))
+
+def k_means(data, k, disFunc, max_iter=300):
     cs = []
     n = data.shape[0]
     for e in enumerate(random.sample(range(n), k)):
@@ -56,7 +102,16 @@ def k_means(data, k, max_iter=300):
         eucDisesList = []
         for j in range(k):
             # eucDises = distance(data, data[0])
-            eucDises = distance(data, cs[j])
+            if(disFunc == 0):
+                eucDises = manhattanDistance(data, cs[j])
+            elif(disFunc == 1):
+                eucDises = euclideanDistance(data, cs[j])
+            elif(disFunc == 2):
+                eucDises = chebyshevDistance(data, cs[j])
+            elif(disFunc == 3):
+                eucDises = chebyshevDistance2(data, cs[j])
+            elif(disFunc == 4):
+                eucDises = cosineDistance(data, cs[j])
             eucDisesList.append(eucDises)
 
         for j in range(n):
@@ -79,7 +134,17 @@ def k_means(data, k, max_iter=300):
   
         conv = True
         for i in range(k):
-            dis2 = distance2(precs[i], cs[i])
+            # dis2 = manhattanP2PDistance(precs[i], cs[i])
+            if(disFunc == 0):
+                eucDises = manhattanP2PDistance(data, cs[j])
+            elif(disFunc == 1):
+                eucDises = euclideanP2PDistance(data, cs[j])
+            elif(disFunc == 2):
+                eucDises = chebyshevP2PDistance(data, cs[j])
+            elif(disFunc == 3):
+                eucDises = chebyshevP2PDistance2(data, cs[j])
+            elif(disFunc == 4):
+                eucDises = cosineP2PDistance(data, cs[j])
             print("Center " +str(i) + " dis2:" + str(dis2))
             if dis2 > 1e-10:
                 conv = False
@@ -89,12 +154,38 @@ def k_means(data, k, max_iter=300):
     return cs, clusters, preds
 
 if __name__ == '__main__':
-    imgNames, imgPreds, imgValues, imgFeatures = loadData("classifier_feature_record.txt")
+    # imgNames, imgPreds, imgValues, imgFeatures = loadData("classifier_feature_record.txt")
+    imgNames, imgPreds, imgValues, imgFeatures = loadData("/Users/cxliu/Documents/Code/CS235/deep_facial_feature_comparison/kmeans/classifier_feature_record.txt")
 
     # imgFeatures = [[1,1,1],[2,2,2],[3,3,3],[4,4,4]]
     # imgFeatures = np.array(imgFeatures, dtype=np.float64)
     start = time.time()
-    centers, clusters, preds= k_means(imgFeatures, 2)
+
+    selectedFeatures = varianceSelection(imgFeatures, imgPreds)
+    trainX = imgFeatures[:, selectedFeatures]
+    for i in range(0, 6):
+        centers, clusters, preds= k_means(trainX, 2, i)
+
+    selectedFeatures = pearsonCorrelationSelection(imgFeatures, imgPreds)
+    trainX = imgFeatures[:, selectedFeatures]
+    for i in range(0, 6):
+        centers, clusters, preds= k_means(trainX, 2, i)
+
+    selectedFeatures = fisherScoreSelection(imgFeatures, imgPreds)
+    trainX = imgFeatures[:, selectedFeatures]
+    for i in range(0, 6):
+        centers, clusters, preds= k_means(trainX, 2, i)
+
+    selectedFeatures = forwardSelection(imgFeatures, imgPreds)
+    trainX = imgFeatures[:, selectedFeatures]
+    for i in range(0, 6):
+        centers, clusters, preds= k_means(trainX, 2, i)
+
+    selectedFeatures = backwardSelection(imgFeatures, imgPreds)
+    trainX = imgFeatures[:, selectedFeatures]
+    for i in range(0, 6):
+        centers, clusters, preds= k_means(trainX, 2, i)
+    
     cnt = 0
     for i in range(len(preds)):
         if(preds[i] == imgPreds[i]):
